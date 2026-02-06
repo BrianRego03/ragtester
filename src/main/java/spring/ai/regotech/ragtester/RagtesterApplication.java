3bb28a544c7596d38bcffc56d7beb48f5b4e0349
brianrego03 <brian.rego03@gmail.com>
2026-02-06 17:43:28 +0530

    Add basic dependencies

---
+++ src/main/java/spring/ai/regotech/ragtester/RagtesterApplication.java
@@ -0,0 +1,13 @@
+package spring.ai.regotech.ragtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagtesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagtesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/ai/regotech/ragtester/RagtesterApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.ai.regotech.ragtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagtesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
