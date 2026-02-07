5f497ad33b9211d429bb7d870cc5afd544ff97ee
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-08 04:02:51 +0530

    Change env dependency and load them before start of application

--- src/main/java/spring/ai/regotech/ragtester/RagtesterApplication.java
+++ src/main/java/spring/ai/regotech/ragtester/RagtesterApplication.java
@@ -1,5 +1,6 @@
 package spring.ai.regotech.ragtester;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RagtesterApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("OLLAMA_URL", dotenv.get("OLLAMA_URL"));
+		System.setProperty("EMBEDDING_MODEL", dotenv.get("EMBEDDING_MODEL"));
+		System.setProperty("CHAT_MODEL", dotenv.get("CHAT_MODEL"));
+		System.setProperty("TABLE_NAME", dotenv.get("TABLE_NAME"));
 		SpringApplication.run(RagtesterApplication.class, args);
 	}
 
