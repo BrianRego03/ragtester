63b2bc22b2e867d1aa179fbb95cc8f7414c514f7
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-07 22:57:47 +0530

    Add basic constructor and variables to Rag service

--- src/main/java/spring/ai/regotech/ragtester/service/RagService.java
+++ src/main/java/spring/ai/regotech/ragtester/service/RagService.java
@@ -1,7 +1,16 @@
 package spring.ai.regotech.ragtester.service;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RagService {
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    public RagService(ChatClient chatClient, VectorStore vectorStore) {
+        this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
+    }
 }
