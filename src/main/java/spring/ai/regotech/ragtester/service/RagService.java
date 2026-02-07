6ab9207d026ee0911043e6729f9be973c55b3025
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-08 00:15:15 +0530

    Add a rag prompt resource object pointing to the string template

--- src/main/java/spring/ai/regotech/ragtester/service/RagService.java
+++ src/main/java/spring/ai/regotech/ragtester/service/RagService.java
@@ -2,12 +2,17 @@ package spring.ai.regotech.ragtester.service;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RagService {
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
+
+    @Value("classpath:/prompts/rag-prompt.st")
+    private Resource ragPromptTemplate;
 
     public RagService(ChatClient.Builder chatClientBuilder, VectorStore vectorStore) {
         this.chatClient = chatClientBuilder.build();
